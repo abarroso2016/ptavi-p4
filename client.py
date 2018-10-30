20475681f10f30a73c952b833bf82eb36cf0c9bb
Andres Barroso Castro <andresbc@beta09.aulas.gsyc.urjc.es>
2018-10-30 13:50:10 +0100

    Modifico el servidor y el cliente para mandar mensajes tipo sip, como indica el apartado 6

--- client.py
+++ client.py
@@ -10,15 +10,16 @@ import sys
 # Constantes. Dirección IP del servidor y contenido a enviar
 SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
-LINE = data = " ".join(sys.argv[3:])
+LINE = data = " ".join(sys.argv[4:])
+TIPO = sys.argv[3].upper()
+TODO = TIPO + " sip:" + LINE + " SIP/2.0"
 
-print(LINE)
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
-    print("Enviando:", LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    print("Enviando:", TODO)
+    my_socket.send(bytes(TODO, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
 
--- server.py
+++ server.py
@@ -7,8 +7,9 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 import socketserver
 import sys
 
+dic = {}
 
-class EchoHandler(socketserver.DatagramRequestHandler):
+class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
@@ -18,15 +19,18 @@ class EchoHandler(socketserver.DatagramRequestHandler):
         handle method of the server class
         (all requests will be handled by this method)
         """
-        self.wfile.write(b"Hemos recibido tu peticion")
+        self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
         for line in self.rfile:
             print("El cliente nos manda ", line.decode('utf-8'))
-            print(self.client_address)
+            sip = line.decode('utf-8').split(" ")
+            dic[sip[1]] = self.client_address[0]
+        for key in dic:
+            print(key + ":" + dic[key])
 
 if __name__ == "__main__":
     # Listens at localhost ('') port 6001 
     # and calls the EchoHandler class to manage the request
-    serv = socketserver.UDPServer(('', int(sys.argv[1])), EchoHandler) 
+    serv = socketserver.UDPServer(('', int(sys.argv[1])), SIPRegisterHandler) 
 
     print("Lanzando servidor UDP de eco...")
     try:
